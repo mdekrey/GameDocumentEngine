9a8ab03599b48d8a732dffc90d5e2c43cb2f1658
Matt DeKrey <mattdekrey@gmail.com>
2023-07-25 23:04:38 -0500

    Update deploy script

--- eng/deployment/deploy.ps1
+++ eng/deployment/deploy.ps1
@@ -1,7 +1,5 @@
 $k8sNamespace='game-docs-engine'
 $releaseName='vtt-dekrey-net'
-$registryLoginServer='dekreydotnet.azurecr.io'
-$imageName='gamedocsengine'
 
 docker build . -t dekreydotnet.azurecr.io/gamedocsengine --build-arg GITHASH=$(git rev-parse HEAD)
 az acr login -n dekreydotnet
@@ -10,6 +8,5 @@ docker push dekreydotnet.azurecr.io/gamedocsengine
 helm repo add mdekrey https://mdekrey.github.io/helm-charts
 helm repo update mdekrey
 helm upgrade --install -n $k8sNamespace $releaseName --repo https://mdekrey.github.io/helm-charts single-container `
-  --set-string "image.repository=$registryLoginServer/$imageName" `
   --set-string "image.tag=latest" `
   --values ./eng/deployment/values.prod.yaml --wait
