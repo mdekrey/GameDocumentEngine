a982a2473815c9bf37e923244e486a280b64a84b
Matt DeKrey <mattdekrey@gmail.com>
2023-07-26 21:42:52 -0500

    Remove unused code

--- GameDocumentEngine.Server/Documents/Types/GameObjectTypeExtensions.cs
+++ GameDocumentEngine.Server/Documents/Types/GameObjectTypeExtensions.cs
@@ -4,7 +4,5 @@ public static class GameObjectTypeExtensions
 {
 	public static string SchemaManifestResourceName(this IGameObjectType gameObjectType) =>
 		$"Documents.Types.{gameObjectType.Key}.schema.json";
-	public static string UiPath(this IGameObjectType gameObjectType) =>
-		$"src/documents/{gameObjectType.Key.ToLower()}/index.ts";
 
 }
