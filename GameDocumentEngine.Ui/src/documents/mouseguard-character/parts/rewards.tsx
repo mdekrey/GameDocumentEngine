72785a2912d9b83d408f864005a26d8e0be35809
Matt DeKrey <mattdekrey@gmail.com>
2023-08-01 21:34:37 -0500

    Implement Rewards

--- GameDocumentEngine.Ui/src/documents/mouseguard-character/parts/rewards.tsx
+++ GameDocumentEngine.Ui/src/documents/mouseguard-character/parts/rewards.tsx
@@ -1,10 +1,20 @@
 import { UseFormResult } from '@/utils/form/useForm';
 import { useFormFields } from '@/utils/form/useFormFields';
 import { CharacterDocument } from '../character-types';
-import { useDebugValue } from 'react';
+import { NumberField } from '@/utils/form-fields/number-field/number-field';
 
 export function Rewards({ form }: { form: UseFormResult<CharacterDocument> }) {
-	const fields = useFormFields(form, {});
-	useDebugValue(fields);
-	return <></>;
+	const fields = useFormFields(form, {
+		fate: ['details', 'rewards', 'fate'],
+		persona: ['details', 'rewards', 'persona'],
+		checks: ['details', 'rewards', 'checks'],
+	});
+
+	return (
+		<>
+			<NumberField.Integer field={fields.fate} description />
+			<NumberField.Integer field={fields.persona} description />
+			<NumberField.Integer field={fields.checks} description />
+		</>
+	);
 }
