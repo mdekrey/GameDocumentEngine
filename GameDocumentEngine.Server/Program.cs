b89566a6c1b176a6e658a087cc491d12e8871124
Matt DeKrey <mattdekrey@gmail.com>
2023-08-08 19:54:27 -0500

    Correct health check path and add icon

--- GameDocumentEngine.Server/Program.cs
+++ GameDocumentEngine.Server/Program.cs
@@ -216,7 +216,7 @@ services.Configure<AspNetCoreInstrumentationOptions>(options =>
 {
 	options.Filter = (httpContext) =>
 	{
-		if (httpContext.Request.Path.Value == "/") return false;
+		if (httpContext.Request.Path.Value?.StartsWith("/health") ?? false) return false;
 		if (httpContext.Request.Path.Value?.StartsWith("/assets") ?? false) return false;
 		if (httpContext.Request.Path.Value?.StartsWith("/hub") ?? false) return false;
 		return true;
