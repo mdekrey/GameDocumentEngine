34c79acd4b91d0fa435d28bd001a9b8ea57e3b46
Matt DeKrey <mattdekrey@gmail.com>
2023-08-10 20:01:09 -0500

    Upgrade to latest fech API adapter

--- GameDocumentEngine.Ui/src/utils/api/fetch-api.ts
+++ GameDocumentEngine.Ui/src/utils/api/fetch-api.ts
@@ -1,22 +1,18 @@
 import { toFetchApi } from '@principlestudios/openapi-codegen-typescript-fetch';
 import operations from '@/api/operations';
 
-export const api = toFetchApi(
-	operations,
-	async (url, req) => {
-		const result = await fetch(url, req);
-		if (result.status === 401) {
-			window.location.href = operations.login.url({
-				returnUrl:
-					window.location.pathname +
-					window.location.search +
-					window.location.hash,
-			});
+export const api = toFetchApi(operations, async (url, req) => {
+	const result = await fetch(url, req);
+	if (result.status === 401) {
+		window.location.href = operations.login.url({
+			returnUrl:
+				window.location.pathname +
+				window.location.search +
+				window.location.hash,
+		});
 
-			// redirecting; cannot be reached
-			throw new Error();
-		}
-		return result;
-	},
-	window.location.origin,
-);
+		// redirecting; cannot be reached
+		throw new Error();
+	}
+	return result;
+});
