836200df6f6da149c5c7afae4745d41760b75ab8
Matt DeKrey <mattdekrey@gmail.com>
2023-08-11 07:01:43 -0500

    Minor tweaks to header layout

--- GameDocumentEngine.Ui/src/components/header/header.tsx
+++ GameDocumentEngine.Ui/src/components/header/header.tsx
@@ -24,69 +24,65 @@ export function Header({ mainItem, menuTabs }: HeaderProps) {
 
 	return (
 		<>
-			<div className="fixed w-full bg-white p-6 border-b border-gray-300 shadow-sm">
-				<div className="flex-none flex flex-row items-center w-full gap-4">
+			<div className="fixed w-full bg-white border-b border-gray-300 shadow-sm flex flex-row items-center gap-4 h-20 px-6">
+				<MenuTabDisplay
+					href="#/"
+					label={t('header.app-title')}
+					icon={HiOutlineDocumentText}
+					labelClassName="font-bold"
+				>
+					{!mainItem && t('header.app-title')}
+				</MenuTabDisplay>
+
+				{mainItem && (
 					<MenuTabDisplay
-						href="#/"
-						label={t('header.app-title')}
-						icon={HiOutlineDocumentText}
+						href={mainItem.href}
+						label={mainItem.label}
 						labelClassName="font-bold"
 					>
-						{!mainItem && t('header.app-title')}
+						{mainItem.label}
 					</MenuTabDisplay>
+				)}
 
-					{mainItem && (
-						<MenuTabDisplay
-							href={mainItem.href}
-							label={mainItem.label}
-							labelClassName="font-bold"
-						>
-							{mainItem.label}
+				<div className="hidden md:contents">
+					{menuTabs?.map(({ href, label }) => (
+						<MenuTabDisplay label={label} href={href} key={href}>
+							{label}
 						</MenuTabDisplay>
-					)}
-
-					<div className="hidden md:contents">
-						{menuTabs?.map(({ href, label }) => (
-							<MenuTabDisplay label={label} href={href} key={href}>
-								{label}
-							</MenuTabDisplay>
-						))}
-					</div>
-
-					<Menu as="div" className="relative inline-block flex-none ml-auto">
-						<div>
-							<Menu.Button
-								className={twMerge(
-									'block w-full justify-center px-2 py-2 text-gray-900 hover:bg-gray-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-700',
-								)}
-								title={t('header.menu')}
-							>
-								<HiBars3 />
-							</Menu.Button>
-						</div>
-
-						<Transition
-							as={Fragment}
-							enter="transition ease-out duration-100"
-							enterFrom="transform opacity-0 scale-95"
-							enterTo="transform opacity-100 scale-100"
-							leave="transition ease-in duration-75"
-							leaveFrom="transform opacity-100 scale-100"
-							leaveTo="transform opacity-0 scale-95"
-						>
-							<Menu.Items className="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-								<div className="px-1 py-1 ">
-									<HeaderMenuLink href="#/game">
-										{t('menu.select-game')}
-									</HeaderMenuLink>
-									<HeaderMenuLink href="#/profile">
-										{t('menu.edit-profile', { name: userQuery.data?.name })}
-									</HeaderMenuLink>
-								</div>
-							</Menu.Items>
-						</Transition>
-					</Menu>
+					))}
 				</div>
+
+				<Menu as="div" className="relative inline-block flex-none ml-auto">
+					<Menu.Button
+						className={twMerge(
+							'block justify-center w-10 h-10 text-center text-gray-900 hover:bg-gray-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-700',
+						)}
+						title={t('header.menu')}
+					>
+						<HiBars3 className="inline-block h-8 w-8" />
+					</Menu.Button>
+
+					<Transition
+						as={Fragment}
+						enter="transition ease-out duration-100"
+						enterFrom="transform opacity-0 scale-95"
+						enterTo="transform opacity-100 scale-100"
+						leave="transition ease-in duration-75"
+						leaveFrom="transform opacity-100 scale-100"
+						leaveTo="transform opacity-0 scale-95"
+					>
+						<Menu.Items className="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+							<div className="px-1 py-1 ">
+								<HeaderMenuLink href="#/game">
+									{t('menu.select-game')}
+								</HeaderMenuLink>
+								<HeaderMenuLink href="#/profile">
+									{t('menu.edit-profile', { name: userQuery.data?.name })}
+								</HeaderMenuLink>
+							</div>
+						</Menu.Items>
+					</Transition>
+				</Menu>
 			</div>
 			<div className="h-20 flex-shrink-0" />
 		</>
@@ -112,7 +108,7 @@ function MenuTabDisplay({
 			aria-label={label}
 			className="flex flex-row items-center gap-1"
 		>
-			{Icon && <Icon />}
+			{Icon && <Icon className="h-8 w-8" />}
 			<span className={twMerge('flex-1', labelClassName)}>{children}</span>
 		</a>
 	);
