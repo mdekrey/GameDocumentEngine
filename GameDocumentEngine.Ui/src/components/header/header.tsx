76196fdf43eecea7664e55b5cc48bfe44cb9daf3
Matt DeKrey <mattdekrey@gmail.com>
2023-08-16 05:52:40 -0500

    Simplify header

--- GameDocumentEngine.Ui/src/components/header/header.tsx
+++ GameDocumentEngine.Ui/src/components/header/header.tsx
@@ -32,52 +32,50 @@ export function Header({
 	const { t } = useTranslation(['layout']);
 
 	return (
-		<>
-			<div
-				className={twMerge(
-					'fixed w-full bg-layout-nav-top text-brand-white shadow-sm flex flex-row items-center gap-4 h-12 p-1',
-					className,
-				)}
+		<div
+			className={twMerge(
+				'w-full bg-layout-nav-top text-brand-white shadow-sm flex flex-row items-center gap-4 h-12 p-1 z-normal',
+				className,
+			)}
+		>
+			<MenuTabDisplay
+				href="#/"
+				label={t('header.app-title')}
+				icon={HiHome}
+				className="z-normal"
+				labelClassName="font-bold"
 			>
-				<MenuTabDisplay
-					href="#/"
-					label={t('header.app-title')}
-					icon={HiHome}
-					labelClassName="font-bold"
-				>
-					{t('header.app-title')}
-				</MenuTabDisplay>
+				{t('header.app-title')}
+			</MenuTabDisplay>
 
-				<div className="flex-1" />
+			<div className="flex-1" />
 
-				<NetworkIndicator
-					connectionState={connectionState}
-					onReconnect={onReconnect}
+			<NetworkIndicator
+				connectionState={connectionState}
+				onReconnect={onReconnect}
+			/>
+
+			<Menu as="div" className="relative block flex-none">
+				<Menu.Button
+					className="block"
+					as={AvatarButton}
+					user={user}
+					title={t('header.menu')}
 				/>
 
-				<Menu as="div" className="relative block flex-none">
-					<Menu.Button
-						className="block"
-						as={AvatarButton}
-						user={user}
-						title={t('header.menu')}
-					/>
-
-					<Transition
-						as={Fragment}
-						enter="transition ease-out duration-100"
-						enterFrom="transform opacity-0 scale-95"
-						enterTo="transform opacity-100 scale-100"
-						leave="transition ease-in duration-75"
-						leaveFrom="transform opacity-100 scale-100"
-						leaveTo="transform opacity-0 scale-95"
-					>
-						<MenuItems menuItems={menuItems} />
-					</Transition>
-				</Menu>
-			</div>
-			<div className={twMerge('h-12 flex-shrink-0', className)} />
-		</>
+				<Transition
+					as={Fragment}
+					enter="transition ease-out duration-100"
+					enterFrom="transform opacity-0 scale-95"
+					enterTo="transform opacity-100 scale-100"
+					leave="transition ease-in duration-75"
+					leaveFrom="transform opacity-100 scale-100"
+					leaveTo="transform opacity-0 scale-95"
+				>
+					<MenuItems menuItems={menuItems} />
+				</Transition>
+			</Menu>
+		</div>
 	);
 }
 
@@ -86,19 +84,21 @@ function MenuTabDisplay({
 	label,
 	children,
 	icon: Icon,
+	className,
 	labelClassName,
 }: {
 	href: string;
 	label: string;
 	children?: React.ReactNode;
 	icon?: IconType;
+	className?: string;
 	labelClassName?: string;
 }) {
 	return (
 		<a
 			href={href}
 			aria-label={label}
-			className="flex flex-row items-center gap-1"
+			className={twMerge('flex flex-row items-center gap-1', className)}
 		>
 			{Icon && <Icon className="h-8 w-8" />}
 			<span className={twMerge('flex-1', labelClassName)}>{children}</span>
