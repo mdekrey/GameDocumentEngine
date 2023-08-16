30c21d75773da2d097bb081e6258c7b59de26783
Matt DeKrey <mattdekrey@gmail.com>
2023-08-16 07:36:41 -0500

    Add fonts to tailwind listing

--- GameDocumentEngine.Ui/src/stories/tailwind-info.ts
+++
@@ -1,8 +0,0 @@
-import tailwindConfig from '../../tailwind.config';
-import resolveConfig from 'tailwindcss/resolveConfig';
-import flattenColorPalette from 'tailwindcss/lib/util/flattenColorPalette';
-
-const fullConfig = resolveConfig(tailwindConfig);
-
-export const colors = flattenColorPalette(fullConfig.theme?.colors);
-export const allColorNames = Object.keys(colors);
