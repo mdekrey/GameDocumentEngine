b856ce28f03c76e981ae6ab3300cd1c40b6ab1a2
Matt DeKrey <mattdekrey@gmail.com>
2023-09-06 22:14:54 -0500

    Improve extra-small screen support

--- GameDocumentEngine.Ui/src/apps/game-details/game-details.tsx
+++ GameDocumentEngine.Ui/src/apps/game-details/game-details.tsx
@@ -17,7 +17,7 @@ export function GameDetails({ gameId }: { gameId: string }) {
 
 	return (
 		<div className="min-h-[10rem] lg:h-max flex flex-row items-center p-4 gap-8 max-w-screen-md">
-			<Arrow className="w-40 h-40 rotate-[-30deg] lg:rotate-[-70deg] flex-shrink-0" />
+			<Arrow className="w-24 h-24 sm:w-40 sm:h-40 -rotate-12 sm:rotate-[-30deg] lg:rotate-[-70deg] flex-shrink-0" />
 			<IntroText className="lg:hidden">{t('mobile-instructions')}</IntroText>
 			<IntroText className="hidden lg:block">
 				{t('sidebar-instructions')}
