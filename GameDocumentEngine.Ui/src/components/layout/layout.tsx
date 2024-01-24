89f67fbe6136f986777920afaf240228c0217ac8
Matt DeKrey <matt.dekrey@principlestudios.com>
2024-01-23 21:09:44 -0600

    Begin presentational layout components

--- GameDocumentEngine.Ui/src/components/layout/layout.tsx
+++ GameDocumentEngine.Ui/src/components/layout/layout.tsx
@@ -2,7 +2,6 @@ import { withSlots } from 'react-slot-component';
 import { Modals, hasOpenModal } from '@/utils/modal/modal-service';
 import type { HeaderProps } from '../header/header';
 import { Header } from '../header/header';
-import type { NetworkIndicatorProps } from '../network/network-indicator';
 import styles from './layout.module.css';
 import { twMerge } from 'tailwind-merge';
 import { HiOutlineEllipsisVertical } from 'react-icons/hi2';
@@ -10,9 +9,9 @@ import { IconButton } from '../button/icon-button';
 import { useEffect, useRef } from 'react';
 import { useLocation } from 'react-router-dom';
 import { useStore } from 'jotai';
+import { elementTemplate } from '../template';
 
-export type LayoutProps = { children?: React.ReactNode } & HeaderProps &
-	NetworkIndicatorProps;
+export type LayoutProps = { children?: React.ReactNode } & HeaderProps;
 
 export type LayoutSlots = {
 	Subheader: {
@@ -40,6 +39,21 @@ function useInert<TElem extends HTMLElement>() {
 	return ref;
 }
 
+const base = elementTemplate('Base', 'section', (T) => (
+	<T tabIndex={0} className="overflow-auto text-slate-950 dark:text-white" />
+));
+const Sidebar = base
+	.extend('Sidebar', (T) => (
+		<T tabIndex={0} className="bg-slate-200 dark:bg-slate-700" />
+	))
+	.themed({
+		Left: (T) => <T className={styles['sidebar-left']} />,
+		Right: (T) => <T className={styles['sidebar-right']} />,
+	});
+const Main = base.extend('Main', () => (
+	<main tabIndex={-1} className="bg-white dark:bg-slate-950" />
+));
+
 export const Layout = withSlots<LayoutSlots, LayoutProps>(function Layout({
 	children,
 	menuItems,
@@ -60,80 +74,88 @@ export const Layout = withSlots<LayoutSlots, LayoutProps>(function Layout({
 	const rootRef = useInert<HTMLDivElement>();
 	return (
 		<>
-			<div
-				className={styles.layout}
-				data-left-drawer={slotProps.LeftSidebar ? true : false}
-				data-right-drawer={slotProps.RightSidebar ? true : false}
-				ref={rootRef}
-			>
-				<Header
-					menuItems={menuItems}
-					user={user}
-					connectionState={connectionState}
-					onReconnect={onReconnect}
-					className={styles.header}
-				>
-					{slotProps.Subheader && (
-						<nav className="hidden lg:block max-w-screen-md flex-1 px-4 py-1 bg-slate-100 dark:bg-slate-900 rounded-sm shadow-inner">
-							{slotProps.Subheader?.children}
-						</nav>
-					)}
-				</Header>
-
-				{slotProps.Subheader && (
-					<nav
-						className={twMerge(
-							styles.subheader,
-							'lg:hidden bg-slate w-full bg-slate-100 text-slate-800 dark:bg-slate-900 dark:text-slate-100 shadow-sm flex flex-row items-center gap-2 h-12 p-1',
-						)}
-					>
-						{slotProps.LeftSidebar && (
-							<IconButton.Secondary
-								className="lg:hidden"
-								onClick={() => leftSidebar?.current?.focus()}
-							>
-								<HiOutlineEllipsisVertical className="h-8 w-8" />
-							</IconButton.Secondary>
-						)}
-						<div className="flex-1">{slotProps.Subheader?.children}</div>
-					</nav>
-				)}
-
-				<section
-					ref={leftSidebar}
-					tabIndex={0}
-					className={twMerge(
-						styles['sidebar-left'],
-						'overflow-auto bg-slate-200 text-slate-950 dark:bg-slate-700 dark:text-white',
-					)}
-				>
-					{slotProps.LeftSidebar?.children}
-				</section>
-				<main
-					tabIndex={-1}
-					className={twMerge(
-						styles.main,
-						'overflow-auto',
-						'bg-white text-slate-950',
-						'dark:bg-slate-950 dark:text-white',
-					)}
-				>
-					{children}
-				</main>
-				<section
-					ref={rightSidebar}
-					tabIndex={0}
-					className={twMerge(
-						styles['sidebar-right'],
-						'overflow-auto bg-slate-200 text-slate-950 dark:bg-slate-700 dark:text-white',
-					)}
-				>
-					{slotProps.RightSidebar?.children}
-				</section>
-
-				<div className={styles.backdrop}></div>
-			</div>
+			<LayoutPresentation
+				slotProps={slotProps}
+				rootRef={rootRef}
+				menuItems={menuItems}
+				user={user}
+				connectionState={connectionState}
+				onReconnect={onReconnect}
+				leftSidebar={leftSidebar}
+				children={children}
+				rightSidebar={rightSidebar}
+			/>
 			<Modals />
 		</>
 	);
 });
+
+function LayoutPresentation({
+	slotProps,
+	menuItems,
+	user,
+	connectionState,
+	onReconnect,
+	children,
+	rootRef,
+	leftSidebar,
+	rightSidebar,
+}: {
+	slotProps: Partial<LayoutSlots>;
+	children?: React.ReactNode;
+	rootRef: React.RefObject<HTMLDivElement>;
+	leftSidebar: React.RefObject<HTMLElement>;
+	rightSidebar: React.RefObject<HTMLElement>;
+} & HeaderProps) {
+	return (
+		<div
+			className={styles.layout}
+			data-left-drawer={slotProps.LeftSidebar ? true : false}
+			data-right-drawer={slotProps.RightSidebar ? true : false}
+			ref={rootRef}
+		>
+			<Header
+				menuItems={menuItems}
+				user={user}
+				connectionState={connectionState}
+				onReconnect={onReconnect}
+				className={styles.header}
+			>
+				{slotProps.Subheader && (
+					<nav className="hidden lg:block max-w-screen-md flex-1 px-4 py-1 bg-slate-100 dark:bg-slate-900 rounded-sm shadow-inner">
+						{slotProps.Subheader?.children}
+					</nav>
+				)}
+			</Header>
+
+			{slotProps.Subheader && (
+				<nav
+					className={twMerge(
+						styles.subheader,
+						'lg:hidden bg-slate w-full bg-slate-100 text-slate-800 dark:bg-slate-900 dark:text-slate-100 shadow-sm flex flex-row items-center gap-2 h-12 p-1',
+					)}
+				>
+					{slotProps.LeftSidebar && (
+						<IconButton.Secondary
+							className="lg:hidden"
+							onClick={() => leftSidebar?.current?.focus()}
+						>
+							<HiOutlineEllipsisVertical className="h-8 w-8" />
+						</IconButton.Secondary>
+					)}
+					<div className="flex-1">{slotProps.Subheader?.children}</div>
+				</nav>
+			)}
+
+			<Sidebar.Left ref={leftSidebar}>
+				{slotProps.LeftSidebar?.children}
+			</Sidebar.Left>
+			<Main className={styles.main}>{children}</Main>
+			<Sidebar.Right ref={rightSidebar}>
+				{slotProps.RightSidebar?.children}
+			</Sidebar.Right>
+
+			<div className={styles.backdrop}></div>
+		</div>
+	);
+}
