86f51d9c4d9d98b240213b17778c2369fa60724c
Matt DeKrey <matt.dekrey@principlestudios.com>
2024-01-30 22:02:41 -0600

    Split Presentation component

--- GameDocumentEngine.Ui/src/apps/documents/subheader/document-subheader.tsx
+++ GameDocumentEngine.Ui/src/apps/documents/subheader/document-subheader.tsx
@@ -4,6 +4,8 @@ import { useTranslation } from 'react-i18next';
 import { displayDocumentSettings } from '../document-settings/document-settings';
 import { useDocument } from '@/utils/api/hooks';
 import { Subheader } from '@/components/subheader/subheader';
+import type { TFunction } from 'i18next';
+import type { DocumentDetails } from '@/api/models/DocumentDetails';
 
 export function DocumentSubheader({
 	gameId,
@@ -15,14 +17,23 @@ export function DocumentSubheader({
 	const { t } = useTranslation(['document-details']);
 	const document = useDocument(gameId, documentId);
 
+	return <DocumentSubheaderPresentation t={t} document={document} />;
+}
+export function DocumentSubheaderPresentation({
+	t,
+	document,
+}: {
+	t: TFunction;
+	document: DocumentDetails;
+}) {
 	return (
 		<Subheader
 			title={document.name}
-			to={`/game/${gameId}/document/${documentId}`}
+			to={`/game/${document.gameId}/document/${document.id}`}
 		>
 			{displayDocumentSettings(document) && (
 				<IconLinkButton.Secondary
-					to={`/game/${gameId}/document/${documentId}/settings`}
+					to={`/game/${document.gameId}/document/${document.id}/settings`}
 					title={t('settings', { name: document.name })}
 				>
 					<HiOutlineCog6Tooth />
--- GameDocumentEngine.Ui/src/apps/game-details/game-subheader.tsx
+++ GameDocumentEngine.Ui/src/apps/game-details/game-subheader.tsx
@@ -4,16 +4,28 @@ import { useTranslation } from 'react-i18next';
 import { displayGameSettings } from '../game-settings/game-settings';
 import { useGame } from '@/utils/api/hooks';
 import { Subheader } from '@/components/subheader/subheader';
+import type { TFunction } from 'i18next';
+import type { GameDetails } from '@/api/models/GameDetails';
 
 export function GameSubheader({ gameId }: { gameId: string }) {
 	const { t } = useTranslation(['game-details']);
 	const gameDetails = useGame(gameId);
 
+	return <GameSubheaderPresentation t={t} game={gameDetails} />;
+}
+
+export function GameSubheaderPresentation({
+	t,
+	game,
+}: {
+	t: TFunction;
+	game: GameDetails;
+}) {
 	return (
-		<Subheader to={`/game/${gameId}`} title={gameDetails.name}>
-			{displayGameSettings(gameDetails) && (
+		<Subheader to={`/game/${game.id}`} title={game.name}>
+			{displayGameSettings(game) && (
 				<IconLinkButton.Secondary
-					to={`/game/${gameId}/settings`}
+					to={`/game/${game.id}/settings`}
 					title={t('edit-game')}
 				>
 					<HiOutlineCog6Tooth />
