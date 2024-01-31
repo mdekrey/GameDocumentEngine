0a9c89fe8fd485b82ec676b85340beb4d9e0868f
Matt DeKrey <matt.dekrey@principlestudios.com>
2024-01-30 22:07:11 -0600

    Extract instructions presentational component

--- GameDocumentEngine.Ui/src/apps/game-details/game-details.tsx
+++ GameDocumentEngine.Ui/src/apps/game-details/game-details.tsx
@@ -2,12 +2,21 @@ import { useTranslation } from 'react-i18next';
 import { IntroText } from '@/components/text/common';
 import { ReactComponent as Arrow } from './arrow.svg';
 import { useGame } from '@/utils/api/hooks';
+import type { TFunction } from 'i18next';
 
 export function GameDetails({ gameId }: { gameId: string }) {
 	// TODO: display things here
 	useGame(gameId);
 	const { t } = useTranslation(['game-details']);
 
+	return <GameDetailsInitialInstructionsPresentation t={t} />;
+}
+
+export function GameDetailsInitialInstructionsPresentation({
+	t,
+}: {
+	t: TFunction;
+}) {
 	return (
 		<div className="min-h-[10rem] lg:h-max flex flex-row items-center p-4 gap-8 max-w-screen-md">
 			<Arrow className="w-24 h-24 sm:w-40 sm:h-40 -rotate-12 sm:rotate-[-30deg] lg:rotate-[-70deg] flex-shrink-0" />
